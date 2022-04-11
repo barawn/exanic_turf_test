b2d81a16e460363fd2f16e330d7927e0fe78b361
Patrick Allison <dbarawn@gmail.com>
2022-04-11 10:27:26 -0400

    update xillybus

--- xillybus/xillybus.v
+++ xillybus/xillybus.v
@@ -2,55 +2,41 @@
 
 module xillybus(PCIE_TX_P, PCIE_TX_N, PCIE_RX_P, PCIE_RX_N, PCIE_REFCLK_P,
   PCIE_REFCLK_N, PCIE_PERST_B_LS, bus_clk, quiesce, GPIO_LED,
-  user_r_read_32_rden, user_r_read_32_data, user_r_read_32_empty,
-  user_r_read_32_eof, user_r_read_32_open, user_w_write_32_wren,
-  user_w_write_32_data, user_w_write_32_full, user_w_write_32_open,
-  user_r_read_8_rden, user_r_read_8_data, user_r_read_8_empty,
-  user_r_read_8_eof, user_r_read_8_open, user_w_write_8_wren,
-  user_w_write_8_data, user_w_write_8_full, user_w_write_8_open,
-  user_r_mem_8_rden, user_r_mem_8_data, user_r_mem_8_empty, user_r_mem_8_eof,
-  user_r_mem_8_open, user_w_mem_8_wren, user_w_mem_8_data, user_w_mem_8_full,
-  user_w_mem_8_open, user_mem_8_addr, user_mem_8_addr_update);
+  user_w_mmreq_wren, user_w_mmreq_data, user_w_mmreq_full, user_w_mmreq_open,
+  user_r_mmresp_rden, user_r_mmresp_data, user_r_mmresp_empty,
+  user_r_mmresp_eof, user_r_mmresp_open, user_r_event_ctrl_rden,
+  user_r_event_ctrl_data, user_r_event_ctrl_empty, user_r_event_ctrl_eof,
+  user_r_event_ctrl_open, user_w_event_out_wren, user_w_event_out_data,
+  user_w_event_out_full, user_w_event_out_open);
 
   input [7:0] PCIE_RX_P;
   input [7:0] PCIE_RX_N;
   input  PCIE_REFCLK_P;
   input  PCIE_REFCLK_N;
   input  PCIE_PERST_B_LS;
-  input [31:0] user_r_read_32_data;
-  input  user_r_read_32_empty;
-  input  user_r_read_32_eof;
-  input  user_w_write_32_full;
-  input [7:0] user_r_read_8_data;
-  input  user_r_read_8_empty;
-  input  user_r_read_8_eof;
-  input  user_w_write_8_full;
-  input [7:0] user_r_mem_8_data;
-  input  user_r_mem_8_empty;
-  input  user_r_mem_8_eof;
-  input  user_w_mem_8_full;
+  input  user_w_mmreq_full;
+  input [31:0] user_r_mmresp_data;
+  input  user_r_mmresp_empty;
+  input  user_r_mmresp_eof;
+  input [7:0] user_r_event_ctrl_data;
+  input  user_r_event_ctrl_empty;
+  input  user_r_event_ctrl_eof;
+  input  user_w_event_out_full;
   output [7:0] PCIE_TX_P;
   output [7:0] PCIE_TX_N;
   output  bus_clk;
   output  quiesce;
   output [3:0] GPIO_LED;
-  output  user_r_read_32_rden;
-  output  user_r_read_32_open;
-  output  user_w_write_32_wren;
-  output [31:0] user_w_write_32_data;
-  output  user_w_write_32_open;
-  output  user_r_read_8_rden;
-  output  user_r_read_8_open;
-  output  user_w_write_8_wren;
-  output [7:0] user_w_write_8_data;
-  output  user_w_write_8_open;
-  output  user_r_mem_8_rden;
-  output  user_r_mem_8_open;
-  output  user_w_mem_8_wren;
-  output [7:0] user_w_mem_8_data;
-  output  user_w_mem_8_open;
-  output [4:0] user_mem_8_addr;
-  output  user_mem_8_addr_update;
+  output  user_w_mmreq_wren;
+  output [31:0] user_w_mmreq_data;
+  output  user_w_mmreq_open;
+  output  user_r_mmresp_rden;
+  output  user_r_mmresp_open;
+  output  user_r_event_ctrl_rden;
+  output  user_r_event_ctrl_open;
+  output  user_w_event_out_wren;
+  output [31:0] user_w_event_out_data;
+  output  user_w_event_out_open;
   wire  trn_reset_n;
   wire  trn_lnk_up_n;
   wire [3:0] s_axis_rq_tready;
@@ -211,19 +197,16 @@ module xillybus(PCIE_TX_P, PCIE_TX_N, PCIE_RX_P, PCIE_RX_N, PCIE_REFCLK_P,
       );
 
   xillybus_core  xillybus_core_ins(.trn_reset_n_w(trn_reset_n),
-    .user_w_mem_8_wren_w(user_w_mem_8_wren), .user_w_mem_8_data_w(user_w_mem_8_data),
-    .user_w_mem_8_full_w(user_w_mem_8_full), .user_w_mem_8_open_w(user_w_mem_8_open),
-    .trn_lnk_up_n_w(trn_lnk_up_n), .user_mem_8_addr_w(user_mem_8_addr),
-    .user_mem_8_addr_update_w(user_mem_8_addr_update), .quiesce_w(quiesce),
-    .GPIO_LED_w(GPIO_LED), .s_axis_rq_tready_w(s_axis_rq_tready),
-    .s_axis_rq_tdata_w(s_axis_rq_tdata), .s_axis_rq_tkeep_w(s_axis_rq_tkeep),
-    .s_axis_rq_tlast_w(s_axis_rq_tlast), .s_axis_rq_tvalid_w(s_axis_rq_tvalid),
-    .s_axis_rq_tuser_w(s_axis_rq_tuser), .m_axis_rc_tdata_w(m_axis_rc_tdata),
-    .m_axis_rc_tkeep_w(m_axis_rc_tkeep), .m_axis_rc_tlast_w(m_axis_rc_tlast),
-    .m_axis_rc_tvalid_w(m_axis_rc_tvalid), .m_axis_rc_tready_w(m_axis_rc_tready),
-    .m_axis_cq_tdata_w(m_axis_cq_tdata), .m_axis_cq_tkeep_w(m_axis_cq_tkeep),
-    .m_axis_cq_tlast_w(m_axis_cq_tlast), .m_axis_cq_tvalid_w(m_axis_cq_tvalid),
-    .m_axis_cq_tready_w(m_axis_cq_tready), .cfg_interrupt_msi_int_w(cfg_interrupt_msi_int),
+    .trn_lnk_up_n_w(trn_lnk_up_n), .quiesce_w(quiesce), .GPIO_LED_w(GPIO_LED),
+    .s_axis_rq_tready_w(s_axis_rq_tready), .s_axis_rq_tdata_w(s_axis_rq_tdata),
+    .s_axis_rq_tkeep_w(s_axis_rq_tkeep), .s_axis_rq_tlast_w(s_axis_rq_tlast),
+    .s_axis_rq_tvalid_w(s_axis_rq_tvalid), .s_axis_rq_tuser_w(s_axis_rq_tuser),
+    .m_axis_rc_tdata_w(m_axis_rc_tdata), .m_axis_rc_tkeep_w(m_axis_rc_tkeep),
+    .m_axis_rc_tlast_w(m_axis_rc_tlast), .m_axis_rc_tvalid_w(m_axis_rc_tvalid),
+    .m_axis_rc_tready_w(m_axis_rc_tready), .m_axis_cq_tdata_w(m_axis_cq_tdata),
+    .m_axis_cq_tkeep_w(m_axis_cq_tkeep), .m_axis_cq_tlast_w(m_axis_cq_tlast),
+    .m_axis_cq_tvalid_w(m_axis_cq_tvalid), .m_axis_cq_tready_w(m_axis_cq_tready),
+    .cfg_interrupt_msi_int_w(cfg_interrupt_msi_int),
     .cfg_interrupt_msi_pending_status_w(cfg_interrupt_msi_pending_status),
     .cfg_interrupt_msi_sent_w(cfg_interrupt_msi_sent),
     .cfg_interrupt_msi_fail_w(cfg_interrupt_msi_fail),
@@ -233,20 +216,18 @@ module xillybus(PCIE_TX_P, PCIE_TX_N, PCIE_RX_P, PCIE_RX_N, PCIE_REFCLK_P,
     .pcie_rq_seq_num1_w(pcie_rq_seq_num1), .pcie_rq_seq_num_vld1_w(pcie_rq_seq_num_vld1),
     .cfg_max_payload_w(cfg_max_payload), .cfg_max_read_req_w(cfg_max_read_req),
     .cfg_rcb_status_w(cfg_rcb_status), .cfg_err_fatal_out_w(cfg_err_fatal_out),
-    .user_r_read_32_rden_w(user_r_read_32_rden), .user_r_read_32_data_w(user_r_read_32_data),
-    .user_r_read_32_empty_w(user_r_read_32_empty),
-    .user_r_read_32_eof_w(user_r_read_32_eof), .user_r_read_32_open_w(user_r_read_32_open),
-    .user_w_write_32_wren_w(user_w_write_32_wren),
-    .user_w_write_32_data_w(user_w_write_32_data),
-    .user_w_write_32_full_w(user_w_write_32_full),
-    .user_w_write_32_open_w(user_w_write_32_open),
-    .user_r_read_8_rden_w(user_r_read_8_rden), .user_r_read_8_data_w(user_r_read_8_data),
-    .user_r_read_8_empty_w(user_r_read_8_empty), .user_r_read_8_eof_w(user_r_read_8_eof),
-    .user_r_read_8_open_w(user_r_read_8_open), .user_w_write_8_wren_w(user_w_write_8_wren),
-    .user_w_write_8_data_w(user_w_write_8_data), .user_w_write_8_full_w(user_w_write_8_full),
-    .user_w_write_8_open_w(user_w_write_8_open), .bus_clk_w(bus_clk),
-    .user_r_mem_8_rden_w(user_r_mem_8_rden), .user_r_mem_8_data_w(user_r_mem_8_data),
-    .user_r_mem_8_empty_w(user_r_mem_8_empty), .user_r_mem_8_eof_w(user_r_mem_8_eof),
-    .user_r_mem_8_open_w(user_r_mem_8_open));
+    .user_w_mmreq_wren_w(user_w_mmreq_wren), .user_w_mmreq_data_w(user_w_mmreq_data),
+    .user_w_mmreq_full_w(user_w_mmreq_full), .user_w_mmreq_open_w(user_w_mmreq_open),
+    .user_r_mmresp_rden_w(user_r_mmresp_rden), .user_r_mmresp_data_w(user_r_mmresp_data),
+    .user_r_mmresp_empty_w(user_r_mmresp_empty), .user_r_mmresp_eof_w(user_r_mmresp_eof),
+    .user_r_mmresp_open_w(user_r_mmresp_open), .user_r_event_ctrl_rden_w(user_r_event_ctrl_rden),
+    .user_r_event_ctrl_data_w(user_r_event_ctrl_data),
+    .user_r_event_ctrl_empty_w(user_r_event_ctrl_empty),
+    .user_r_event_ctrl_eof_w(user_r_event_ctrl_eof),
+    .user_r_event_ctrl_open_w(user_r_event_ctrl_open),
+    .user_w_event_out_wren_w(user_w_event_out_wren),
+    .user_w_event_out_data_w(user_w_event_out_data),
+    .user_w_event_out_full_w(user_w_event_out_full),
+    .user_w_event_out_open_w(user_w_event_out_open), .bus_clk_w(bus_clk));
 
 endmodule
