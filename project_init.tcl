ccb0e2f85067b214b73095b468cab046a9409ab0
Patrick Allison <dbarawn@gmail.com>
2022-05-04 14:11:30 -0400

    fix project init/pre synthesis scripts

--- project_init.tcl
+++ project_init.tcl
@@ -1,5 +1,15 @@
-set f [open "barawn_repository"]
+proc get_repo_dir {} {
+    set projdir [get_property DIRECTORY [current_project]]
+    set projdirlist [ file split $projdir ]
+    set basedirlist [ lreplace $projdirlist end end ]
+    return [ file join {*}$basedirlist ]
+}
+		  
+set f [open [ file join [get_repo_dir] "barawn_repository" ] ]
 set repo [read $f]
 puts "Updating IP repo with ${repo}"
 set_property ip_repo_paths $repo [current_project]
- 
+puts "Updating pre init script"
+set pre [ file join [get_repo_dir] "pre_synthesis.tcl"]
+set_property STEPS.SYNTH_DESIGN.TCL.PRE [ get_files $pre -of [get_fileset utils_1] ] [get_runs synth_1]
+
