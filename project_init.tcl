481e9eccbf23d281d562a143c528413c2c4b77f5
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2023-05-04 10:26:57 -1000

    update init/deinit and gitignore

--- project_init.tcl
+++ project_init.tcl
@@ -6,23 +6,18 @@ proc get_repo_dir {} {
     return [ file join {*}$basedirlist ]
 }
 
+# source utilities
+source [file join [get_repo_dir] "submodules" "verilog-library-barawn" "tclbits" "utility.tcl"]
+# source repo control
+source [file join [get_repo_dir] "submodules" "verilog-library-barawn" "tclbits" "repo_files.tcl"]
+
 # Make sure the project behavior stays the same.
 set f [open [ file join [get_repo_dir] "barawn_repository" ] ]
 set repo [read $f]
-if {$repo in [get_property ip_repo_paths [current_project]]} {
-    puts "Skipping IP repo update, already done"
-} else {
-    puts "Updating IP repo with ${repo}"
-    set_property ip_repo_paths $repo [current_project]
-}
+add_ip_repository $repo
 
-# check for pre-init script
-set pre [ file join [get_repo_dir] "pre_synthesis.tcl"]
-if {$pre in [get_files -of_objects [get_filesets utils_1]]} {
-    puts "Skipping pre init script update"
-} else {
-    puts "Updating pre init script"
-    add_files -fileset utils_1 -norecurse $pre
-    set_property STEPS.SYNTH_DESIGN.TCL.PRE [ get_files $pre -of [get_fileset utils_1] ] [get_runs synth_1]
-}
+# Add pre-synthesis script if needed
+set_pre_synthesis_tcl "pre_synthesis.tcl"
 
+# And check if everything's loaded.
+check_all
